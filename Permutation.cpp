a129c65eec2543eee81d5bf80282ab2e0012ebd4
Meena Erian Awad Hanna <meena5.erian@gmail.com>
2016-11-23 04:42:51 +0200

    Update Permutation.cpp

--- Permutation.cpp
+++ Permutation.cpp
@@ -6,17 +6,20 @@ int main()
 start:
     std::cout<<"\nnPr ";
     std::cin>>n>>r;
-    std::cout<<"\n"<<n<<"P"<<r<<"="<<nPr(n,r)<<std::endl;
-    std::cout<<"Print all possible permutations? (y,n) : ";
-    std::cin.get();
-    if(std::cin.get()=='y')
-    PrintAllPossiblePermutations(n,r);
-    std::cin.get();
-    //PrintAllPossiblePermutations(n,r);
-    std::cout<<"\nGetPermutation : PermutationID = ";
-    unsigned long PID;
-    std::cin>>PID;
-    std::cout<<"\nPermutation["<<PID<<"]="<<GetPermutation(n,r,PID);
-    std::cout<<"\n\n\n\n";
+    if(n>r)std::cout<<"LOGIC ERROR: (n>r)\n";
+    else
+    {
+        std::cout<<"\n"<<n<<"P"<<r<<"="<<nPr(n,r)<<std::endl;
+        std::cout<<"Print all possible permutations? (y,n) : ";
+        std::cin.get();
+        if(std::cin.get()=='y')
+        PrintAllPossiblePermutations(n,r);
+        std::cin.get();
+        std::cout<<"\nGetPermutation : PermutationID = ";
+        unsigned long PID;
+        std::cin>>PID;
+        std::cout<<"\nPermutation["<<PID<<"]="<<GetPermutation(n,r,PID);
+        std::cout<<"\n\n\n\n";
+    }
 goto start;
 }
