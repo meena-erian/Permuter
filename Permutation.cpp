ac28d260f33b1c6a0d970e2a59259c8e8f4ca38f
Meena Erian Awad Hanna <meena5.erian@gmail.com>
2016-11-23 04:35:25 +0200

    Create Permutation.cpp

---
+++ Permutation.cpp
@@ -0,0 +1,22 @@
+#include "Permutation.h"
+
+int main()
+{
+    unsigned n,r;
+start:
+    std::cout<<"\nnPr ";
+    std::cin>>n>>r;
+    std::cout<<"\n"<<n<<"P"<<r<<"="<<nPr(n,r)<<std::endl;
+    std::cout<<"Print all possible permutations? (y,n) : ";
+    std::cin.get();
+    if(std::cin.get()=='y')
+    PrintAllPossiblePermutations(n,r);
+    std::cin.get();
+    //PrintAllPossiblePermutations(n,r);
+    std::cout<<"\nGetPermutation : PermutationID = ";
+    unsigned long PID;
+    std::cin>>PID;
+    std::cout<<"\nPermutation["<<PID<<"]="<<GetPermutation(n,r,PID);
+    std::cout<<"\n\n\n\n";
+goto start;
+}
